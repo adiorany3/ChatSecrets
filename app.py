ec04aabe39298044b78e916be583af3d793a8feb
Galuh Adi Insani <adioranye@gmail.com>
2026-03-30 11:57:17 +0700

    enkripsi pesan

--- app.py
+++ app.py
@@ -276,7 +276,7 @@ else:
     st.info("Masukkan nama room dan username untuk mulai chat.")
 
 
-st.caption("Software ini dibuat dengan 100% python, dan memanfaatkan teknologi Streamlit. 🔒 Pesan bersifat temporary, dan akan hilang dengan cara destroy atau saat server reboot. Jangan lupa hapus session dan chat room setelah selesai dengan fitur Destroy Chat Room.")
+st.caption("Software ini dibuat dengan 100% python, dan memanfaatkan teknologi Streamlit. 🔒 Pesan dienkripsi dan bersifat temporary, dan akan hilang dengan cara destroy atau saat server reboot. Jangan lupa hapus session dan chat room setelah selesai dengan fitur Destroy Chat Room.")
 
 # Tombol manual refresh chat
 if room and username:
